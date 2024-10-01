75f521201d9d21a072e02d1e28dfd8d6d7e759ea
odette <odegarci@ucsc.edu>
2024-10-01 11:10:39 -0700

    Add files via upload
    now it has something

---
+++ hello-world.py
@@ -0,0 +1,18 @@
+#This program will write:
+#Hello World!
+
+
+#This function tells the
+#computer to print
+#"Hello World"
+def PrintHelloWorld():
+    print("Hello World :3")
+    
+#This defines out main()
+#function for our program
+def main():
+    PrintHelloWorld()
+#When we run the program runs
+#this executes first
+if __name__=="__main__":
+    main()
