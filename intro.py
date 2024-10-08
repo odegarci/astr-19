3d6ce433b729abd0a34c21a6695eb74ad88eddb2
odette <odegarci@ucsc.edu>
2024-10-08 12:29:21 -0700

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,5 @@
+print("Odette Garcia")
+
+print("Any Pronouns are fine")
+
+print("I do not have a favorite movie nor a favorite food. I prefer watching shows and I will eat most of what is given to me.")
