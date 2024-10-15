888492f92558ac689b838dffcdc9f5310dd58534
odette <odegarci@ucsc.edu>
2024-10-15 12:27:56 -0700

    Add files via upload

---
+++ function.py
@@ -0,0 +1,19 @@
+def f(x):
+    return x**3 + 8
+
+def main():
+    result = f(9) 
+    print(result)
+    if result > 27:
+        print("YAY!")
+
+#print("x**3+8")
+
+#result = f(x)
+
+#y = f(9)
+#print (result)
+
+    
+if __name__=="__main__":
+    main()
